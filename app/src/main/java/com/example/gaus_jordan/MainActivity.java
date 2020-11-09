db7924ae79645d2a1ceb0a0003a9b7d30d58959d
”ClemensR” <clemensrichter94@gmail.com>
2020-11-09 17:02:04 +0100

    Passing input of matrix size to next activity.

--- app/src/main/java/com/example/gaus_jordan/MainActivity.java
+++ app/src/main/java/com/example/gaus_jordan/MainActivity.java
@@ -7,10 +7,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_COLS = "com.example.gaus_jordan.EXTRA_COLS";
+    public static final String EXTRA_ROWS = "com.example.gaus_jordan.EXTRA_ROWS";
     private Button button;
+    private EditText matrix_cols;
+    private EditText matrix_rows;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +23,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
+        matrix_cols = findViewById(R.id.editTextNumber);
+        matrix_rows = findViewById(R.id.editTextNumber2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openActivity2();
+
+                openInputActivity();
             }
         });
     }
-    public void openActivity2(){
+    public void openInputActivity(){
+        int cols = Integer.parseInt(matrix_cols.getText().toString());
+        int rows = Integer.parseInt(matrix_rows.getText().toString());
         Intent intent = new Intent(this, MatrixActivity.class);
+
+        intent.putExtra(EXTRA_COLS,cols);
+        intent.putExtra(EXTRA_ROWS,rows);
+
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/gaus_jordan/MatrixActivity.java
+++ app/src/main/java/com/example/gaus_jordan/MatrixActivity.java
@@ -2,13 +2,20 @@ package com.example.gaus_jordan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MatrixActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matrix);
+
+        Intent intent = getIntent();
+        int cols = intent.getIntExtra(MainActivity.EXTRA_COLS, 0);
+        int rows = intent.getIntExtra(MainActivity.EXTRA_ROWS, 0);
+
     }
 }
